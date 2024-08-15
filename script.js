e6a15be17cec2f993e9293af75894ad0ed0e357f
Regadius <66080112+regadius@users.noreply.github.com>
2024-08-15 13:26:36 +0300

    Add logic to get player input

--- script.js
+++ script.js
@@ -1,6 +1,30 @@
-'use strict'
+"use strict";
+// SET playerWins to 0
+let playerWins = 0;
+// SET computerWins to 0
+let computerWins = 0;
+// - SET playerChoice to getPlayerChoice
+let playerChoice = getPlayerChoice();
+// - SET computerChoice to getComputerChoice
+// let computerChoice = getComputerChoice();
+// - CALL playRound with playerChoice and computerChoice
+// - PRINT winnerMessage
 
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3);
-    return randomNumber;
+  let randomNumber = Math.floor(Math.random() * 3);
+  return randomNumber;
+}
+function getPlayerChoice() {
+  let keepGoing = true;
+  do {
+    const playerChoice = prompt("Enter rock, paper or scissors").toLowerCase();
+    if (
+      playerChoice == "rock" ||
+      playerChoice == "paper" ||
+      playerChoice == "scissors"
+    ) {
+      keepGoing = false;
+      return playerChoice;
+    }
+  } while (keepGoing);
 }
